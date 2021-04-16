77971cbedb1e9f0a566671363cdeb7e4bb400e13
asimakiskydros <82280968+asimakiskydros@users.noreply.github.com>
2021-04-16 20:15:37 +0300

    Update BinarySearchTree.h

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -3,21 +3,25 @@
 
 struct node{
 	char *data;
+	int instances;
 	node *left,*right;
 };
 
 class BinarySearchTree{
 	private:
 		node *root;
+		void insert(char*,node*);
+		node *search(char*,node*);
 	public:
 		BinarySearchTree();
-		BinarySearchTree(char*);
+		BinarySearchTree(char*);//probably unimportant?
 		~BinarySearchTree();
 		virtual void insert(char *);
 		node* search(char *);//Searches for string, if it exists
-		//returns its address if not returns NULL (Probably, i might change it)
+		//returns its address and prints its details, if not returns NULL
 		void destroy();//Destroys the tree in its entirety from the top down
-		virtual void delete_node(node *);//Finds then deletes the specified node
+		virtual bool delete_node(node *);//Finds then deletes the specified node
+		//then returns true - if it doesnt exist it returns false
 		void preorder();
 		void inorder();
 		void postorder();
