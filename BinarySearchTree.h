4fa77e956634c5f0dff8b29b5c220ccc6e970c78
asimakiskydros <82280968+asimakiskydros@users.noreply.github.com>
2021-04-19 20:36:42 +0300

    Update BinarySearchTree.h

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -10,25 +10,23 @@ struct node{
 class BinarySearchTree{
 	private:
 		node *root;
-		void insert(char*,node*);
-		node *search(char*,node*);
-		void destroy(node*);
+		void insert(char*,node* /*root*/);
+		node *search(char*,node* /*root*/,node* /*parent*/);
+		void destroy(node* /*root*/);
 	public:
 		BinarySearchTree();//Creates empty tree
-		BinarySearchTree(char*);//Creates tree with only one component, the given string
-		//(probably unimportant for the purposes of this project)
 		~BinarySearchTree();
 		virtual void insert(char *);
 		node* search(char *);//Searches for string, if it exists
 		//returns its address and prints its details, if not returns NULL
 		void destroy();//Destroys the tree in its entirety from the bottom up
-		virtual bool delete_node(node *);//Finds then deletes the specified node
+		virtual bool delete_(char *);//Finds then deletes the specified string
 		//then returns true - if it doesnt exist it returns false
 		void preorder();
 		void inorder();
 		void postorder();
 		void levelorder();
-		node* get_max();//Finds and prints the MAX value then returns its adress
+		node* get_max();//Finds and prints the MAX value then returns its address
 		node* get_min();//Works accordingly, for the MIN value
 };
 
