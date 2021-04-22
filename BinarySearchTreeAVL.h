c9522d3c2750c75576c3e888f80907f8d53dfa22
asimakiskydros <82280968+asimakiskydros@users.noreply.github.com>
2021-04-22 21:17:51 +0300

    Update BinarySearchTreeAVL.h

--- BinarySearchTreeAVL.h
+++ BinarySearchTreeAVL.h
@@ -4,19 +4,20 @@
 
 class BinarySearchTreeAVL:public BinarySearchTree{
 	private:
-		int subRightTreeHeight, subLeftTreeHeight;
-		void checkHeightDifference();
-		void leftRotation();
-		void rightRotation();
+		int treeHeight(node*);
+		int heightDiff(node*);
+		bool checkForUnbalance(node*);//idk
+		void correct(node*);
+		void leftRotation(node*);
+		void rightRotation(node*);
 	public:
+		BinarySearchTreeAVL();
+		~BinarySearchTreeAVL();
 		void insert(char *);
-		void delete_node(node *);
+		bool delete_(char *);
 		//These two behave the same as in the regular tree, but with every
 		//instance they check if the height balance is disturbed and act accordingly
 };
-
-//prosorina templates kai ta 2 logika tha allaksoun kapoia pragmata
-//an exeis na prostheseis kati grapsto
 
 #endif //BINARYSEARCHTREEAVL_H
 
