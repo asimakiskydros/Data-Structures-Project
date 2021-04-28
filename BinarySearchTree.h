35bb66f49bf8cd70a6ec21b4e0025cbb53abce35
asimakiskydros <82280968+asimakiskydros@users.noreply.github.com>
2021-04-28 19:06:59 +0300

    Update BinarySearchTree.h
    Done

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -10,25 +10,26 @@ struct node{
 class BinarySearchTree{
 	protected:
 		node *root;
-		void insert(char*,node* /*root*/);
-		node *search(char*,node* /*root*/,node* /*parent*/,bool* /*isRight?*/);
-		void destroy(node* /*root*/);
-		void print(node* /*root*/);
-		void scan(node* /*root*/,short );//short: 1;preorder 2;inorder 3;postorder
+		void insert(char*,node*);
+		node *search(char*,node*);
+		void destroy(node*);
+		void print(node*);
+		void scan(node*,short);//short: 1;preorder 2;inorder 3;postorder
+		node *parent(char*,node*, bool* /*isRight*/);
 	public:
 		BinarySearchTree();//Creates empty tree
 		~BinarySearchTree();
 		virtual void insert(char *);
 		node* search(char *);//Searches for string, if it exists
 		//returns its address and prints its details, if not returns NULL
-		void destroy();//Destroys the tree in its entirety from the bottom up
+		void reset();//Resets the tree to an empty one (deletes everything and sets root to NULL)
 		virtual bool delete_(char *);//Finds then deletes the specified string
 		//then returns true - if it doesnt exist it returns false
 		void preorder();
 		void inorder();
 		void postorder();
-		node* get_max();//Finds and prints the MAX value then returns its address
-		node* get_min();//Works similarly, for the MIN value
+		char* get_max();//Finds the MAX value then returns its address
+		char* get_min();//The exact same thing but for the MIN value
 };
 
 #endif //BINARYSEARCHTREE_H
