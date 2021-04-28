e5769c0d26235e4fd3a8e2e6519c0adb88143a70
asimakiskydros <82280968+asimakiskydros@users.noreply.github.com>
2021-04-28 20:21:44 +0300

    Update sortedArray.h

--- sortedArray.h
+++ sortedArray.h
@@ -9,7 +9,7 @@ class sortedArray: public unsortedArray
   
   public:
   sortedArray();
-  ~sortedArray();//will probably delete the array
+  ~sortedArray();//will probably delete the array// !!!!!!!****
   void sort();//sorts the array
   bool search(char *)//Looks for the word with binary search, returns true or false depending on if the word is found or not. 
   bool delete_word(char *);//Deletes a specific word in the array and returns true, if the word isn't found returns false
@@ -21,8 +21,10 @@ class sortedArray: public unsortedArray
 
 
 
-
-
-
 //ASIMAKI SAMA, HALP IF WHEREVER HALP IS NEEDED, THENK U!
 //to exo psilosnobari to unsorted array, tha to doulepso me to pou nioso oti teliosa kapos to sorted array..
+
+//Asimakis:
+//****!!!: min kaneis destructor ama i klasi klironomei apo to unsorted, tha fas megalo goal apo ton compiler. Ousiastika ta paidia twn klasewn xrisimopoioun ton destructor tis
+//base klasis opote ama to kaneis tha ta diagrapsei 2 fores kai tha fas SegFault error i DoubleFree.
+//Episis, opws se volevei kane ti douleia sou profanws, apla prosexe ama einai na doulepseis prwta to paidi-class kai meta ton patera gia profaneis logous
