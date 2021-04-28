3676ad1c3aa53c875e9a338d8cc60a0f308ca031
asimakiskydros <82280968+asimakiskydros@users.noreply.github.com>
2021-04-28 20:07:05 +0300

    Update unsortedArray.cpp

--- unsortedArray.cpp
+++ unsortedArray.cpp
@@ -19,7 +19,9 @@ unsortedArray::~unsortedArray()
   delete sum;
   delete tempWords;//i think i dont need to do this
   delete tempSum;//i think that i dont need to do this x2
-}
+}//Asimakis: giati na min xriazetai? rwtaw giati an ta diagrafeis kapou allou kai mpoun ston destructor kena tha fas SegFault (speaking from experience xdd)
+//Sigourepse oti kouvaloun desmeumeni mnimi otan mpainoun ston destructor kai meta na ta kaneis delete - kata t'alla delete xriazontai ola, nai
+
 
 void unsortedArray::insert(char* string)
 {
@@ -61,7 +63,10 @@ void unsortedArray::insert(char* string)
   {
     sum[indx]++;
   }
-}
+}//Asimakis: etsi opws ta vlepw dn mporw na vrw kapoio fanero lathos - char ** xrisimopoiousa ki egw stis askiseis tou lentza me ta strings opote eimai arketa confident oti
+//doulevei. To mono pou tha sou protina einai an thes na auksaneis to megethos tou array kata 5-10 kathe fora pou exeis ftasei sto limit kai oxi kata 1 kathe fora pou kaneis insert
+//Kanei ton kwdika pio grigoro - kai den se xalaei ama telika den xrisimopoihthoun kapoies theseis giati max 4-9 theseis tha minoun kenes se olo to programma kai exoume na kanoume
+//me keimeno 10000+ leksewn xd
 
 
 //PRIVATE, I NEED THIS IN ORDER TO KNOW IF I SHOULD UPDATE THE ARRAYS OR NOT.
@@ -106,8 +111,8 @@ void unsortedArray::search(char* string)
     {
       return;
     }
-  }
-}
+  }//Asimakis: min nomizeis oti egw ktlvenw ti kanw me ta high resolution clock xd, ki egw copy pasta ta ekana. Dn ta exeis parei ola omws, sou dinw to arthro p diavasa
+}//gia na ktlveis pos prepei na ta valeis ston kwdika: https://www.geeksforgeeks.org/measure-execution-time-function-cpp/
 
 
 bool unsortedArray::delete_word(char * string)
