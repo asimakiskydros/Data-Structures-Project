161e1d110b3514de7d035597261bbed24e4fbdcf
GogaC777 <82280875+GogaC777@users.noreply.github.com>
2021-05-11 19:49:00 +0300

    Update unsortedArray.h

--- unsortedArray.h
+++ unsortedArray.h
@@ -5,26 +5,31 @@
 class unsortedArray
 {
   protected:
-  // parallel arrays
+  //PARALLEL ARRAYS
   char **words;//array that stores the unique words dynamically
   int *sum;//array that stores the number of times each unique has appeared
-  char **tempWords;//helps me grow the size of the words array
-  int *tempSum;//helps me grow the size of the sum array
+  char **tempWords;//helps me increase the size of the words array
+  int *tempSum;//helps me increase the size of the sum array
   
-  int indx;//index of the found word, after performing searching.
+  int *length;//holds the length of each unique word, I need this in order to increase the size of the words array and for possible sorting in the future
+  int *tempLength;//helps me increase the size of the length array
   
+  int indx;//index of the found word after performing linear searching.
+  
+  int arr_size; //holds the size of the array and grows it by 5 whenever needed
   int last_word;//holds the index of the last word in the array
   int i;//helps me with loops
 
-  bool wordExists(char *);//Looks for the word with linear search, returns true or false depending on if the word is found or not.
+  virtual bool wordExists(char *);//Looks for the word with linear search, returns true or false depending on if the word is found or not.
 
-  bool firstWord;// I need this to control the value of last_word, basically if I dont use this, last_word's value might increase without getting a new word.
+  bool firstWord;//I need this to control the value of last_word, basically if I dont use this, last_word's value might increase without getting a new word.
+ 
 
   public:
-  unsortedArray();//initializes array ptr to null
+  unsortedArray();//initializes the pointers to null and some helpful variables to 0
   ~unsortedArray();//destroys the array
   virtual void insert(char *);//Inserts the unique words in the array, if the given word already exists, nothing is inserted and only the sum array is updated
-  virtual void search(char *);//Looks for the word with linear search, shows the times the word has appeared(if it exists) plus the searching time
+  void search(char *);//Looks for the word with linear search, shows the times the word has appeared(if it exists) plus the searching time
   virtual bool delete_word(char *);//Deletes a specific word in the array and returns true, if the word isn't found returns false
 
 
@@ -34,15 +39,8 @@ class unsortedArray
 
 
 
-//tha valo parapano comments, apla niotho oti den ine sosta akoma...
 
-//efxome to char **words na ine sosto....
+//O destructor pistevis oti einai sostos tora? Diavasa to comment sou kai elpizo na katalava afto pou ennois ;-;
+//ekana update to insert, isos ine ligo kalitero tora me vasi afta pou mou protines, ty bro. ise 5head
 
-//to anevazo tora stis 11:50 alla tha to ksananevaso simera pali updated(edit: nevermind that... goodnight)
-
-//ASIMAKI SAMA an lipi kati lend me some of your precious knowledge ukno what im sayin
-
-//den vrika poli xrono na doulepso aftes tis meres coz my aunt broke her leg (sad life..) 
-//kai tha prospathiso na kano kai ego speedrun
-
-//LOGIKA EXO LATHI.... AN DIS KATI TRAGIKO PES MOU APEFTHIAS. TY. GOODNIGHT AN TO VLEPIS TORA XD
+//an dis kati pou den sou aresi, afise sxolio xD
